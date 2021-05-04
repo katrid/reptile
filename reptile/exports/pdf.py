3384297e8eba6f175f7ec73c7e0d2305df3ac3d7
alexandre <alexandre@katrid.com>
2021-05-04 13:28:24 -0300

    Initial commit

--- reptile/exports/pdf.py
+++ reptile/exports/pdf.py
@@ -1,38 +1,49 @@
-from PySide2.QtGui import QPainter, QPdfWriter, QFont
-from PySide2.QtCore import QMarginsF, QSizeF
-from reptile.core import mm
+from PySide6.QtGui import QPainter, QFont, QGuiApplication
+from PySide6.QtPrintSupport import QPrinter
+from PySide6.QtCore import QMarginsF, QSizeF, QSize
+from reptile.runtime import PreparedBand
+from reptile.qt import BandRenderer, TextRenderer
+from reptile.units import mm
 
 
-class Export:
+class QReportEngine:
+    app = None
+
+
+class PDF:
     def __init__(self, document):
+        if not QReportEngine.app:
+            QReportEngine.app = QGuiApplication([])
         self.document = document
         self.printer = None
         self.painter = None
-        self._is_first_page = False
+        self._isFirstPage = False
 
     def export(self, filename):
-        self.printer = QPdfWriter(filename)
+        self.printer = QPrinter()
+        self.printer.setOutputFileName(filename)
         self.printer.setPageMargins(QMarginsF(0, 0, 0, 0))
-        self.printer.setResolution(96)
-        if self.document.pages:
-            page = self.document.pages[0]
-            self.printer.setPageSizeMM(QSizeF(page.width / mm, page.height / mm))
+        self.printer.setResolution(72)
+        pages = self.document['pages']
+        if pages:
+            page = pages[0]
+            self.printer.setPageSize(QSize(page.width, page.height))
         self.painter = QPainter()
         self.painter.begin(self.printer)
-        self._is_first_page = True
-        for i, page in enumerate(self.document.pages):
-            self.export_page(page)
-            self._is_first_page = False
+        self._isFirstPage = True
+        for page in pages:
+            self.exportPage(page)
+            self._isFirstPage = False
         self.painter.end()
 
-    def export_page(self, page):
-        if not self._is_first_page:
+    def exportPage(self, page):
+        if not self._isFirstPage:
             self.printer.newPage()
         for band in page.bands:
-            self.export_band(band)
+            self.exportBand(band)
 
-    def export_band(self, band):
-        band.draw(self.painter)
+    def exportBand(self, band: PreparedBand):
+        BandRenderer.draw(band, self.painter)
         for obj in band.objects:
-            obj.draw(self.painter)
+            TextRenderer.draw(band.left, band.top, obj, self.painter)
 
