8ea33995a000c9f87e30cf307748f4ab96a6659a
alexandredias <alexandre@katrid.com>
2024-03-05 01:10:05 -0300

    barcode bug fix

--- reptile/bands/barcodes.py
+++ reptile/bands/barcodes.py
@@ -7,39 +7,47 @@ from barcode.writer import SVGWriter
 
 from reptile.bands.widgets import BandObject, TAG_REGISTRY
 
+
 class Barcode(BandObject):
     element_type = 'barcode'
     code: str = None
     barcode_type = 'code128'
+    show_text = True
     _datasource = None
 
     def prepare(self, stream: List, context):
         from barcode.writer import ImageWriter
-        from reptile.runtime import PreparedImage, SizeMode
+        from reptile.runtime import PreparedBarcode, SizeMode, PreparedImage
+        from reptile.barcodes import code128
         img = PreparedImage()
-        img.size_mode = SizeMode.ZOOM
+        img.size_mode = SizeMode.CENTER
         img.left = self.left
         img.top = self.top
         img.height = self.height
         img.width = self.width
+        # img.bar_width = 0.2  # 0.2mm
+        if self.barcode_type == 'code128':
+            Code = Code128
+        elif self.barcode_type == 'ITF-14':
+            Code = ITF
         if self.field:
             if self._datasource:
-                code = context[self._datasource][self.field]
+                code = context[self.datasource_name][self.field]
                 if code is not None:
-                    Code = None
-                    if self.barcode_type == 'code128':
-                        Code = Code128
-                    elif self.barcode_type == 'ITF-14':
-                        Code = ITF
-                    s = BytesIO()
-                    Code(code, writer=ImageWriter()).write(s, options={'write_text': False, 'quiet_zone': 5.0, 'dpi': 300})
-                    s.seek(0)
-                    img.picture = s.read()
+                    # s = BytesIO()
+                    # Code(code, writer=ImageWriter()).write(s, options={'write_text': self.show_text, 'dpi': 300})
+                    img.picture = code128.get_png(code128.get_barcode(code), width=self.width, height=self.height)
             else:
                 warnings.warn('Datasource not found')
         else:
-            img.picture = self.code
+            img.data = Code(self.code).encoded
         stream.append(img)
+
+    @property
+    def datasource_name(self):
+        if isinstance(self._datasource, str):
+            return self._datasource
+        return self._datasource.name
 
     @property
     def datasource(self):
@@ -56,6 +64,7 @@ class Barcode(BandObject):
         self.field = structure.get('field')
         self._datasource = structure.get('datasource')
         self.barcode_type = structure.get('barcodeType')
+        self.show_text = structure.get('showText', True)
 
 
 TAG_REGISTRY['barcode'] = Barcode
